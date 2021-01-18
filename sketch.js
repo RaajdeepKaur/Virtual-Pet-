bfa6eb224799ea87d1e4ebf8752b0cfa50737b4e
Raajdee[ kaur <71049195+RaajdeepKaur@users.noreply.github.com>
2021-01-18 16:54:24 +0530

    files uploaded

---
+++ sketch.js
@@ -0,0 +1,28 @@
+//Create variables here
+var dog , happyDog
+var database 
+var foodS , foodStock 
+var dogImg, happyDogImg 
+
+function preload()
+{
+  //load images here
+  dogImg = loadImage("images/dogImg.png");
+  happyDogImg = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+	createCanvas(500, 500);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
